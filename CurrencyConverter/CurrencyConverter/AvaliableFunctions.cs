fe11507833103a34527fce4ee8e7e8a39989c9b8
Tsukidai <asb147@gmail.com>
2015-11-17 14:07:29 +0000

    Was doing coding based of the class diagram, and realised that the class name 'strength' was wrong and changed it to 'comparison'.

---
+++ CurrencyConverter/CurrencyConverter/AvaliableFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    public abstract class AvaliableFunctions
+    {
+        private string _chosenCurrency;
+        public string chosenCurrency
+        {
+            get { return _chosenCurrency; }
+            set { _chosenCurrency = value; }
+        }
+
+    }
+}
