924612f510fe74bcab8417112a20ab6ced4f2781
LordStrange <victoryeates@googlemail.com>
2015-11-17 16:55:07 +0000

    Added working XML plus fews changes to program

--- CurrencyConverter/CurrencyConverter/Program.cs
+++ CurrencyConverter/CurrencyConverter/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace CurrencyConverter
 {
@@ -10,6 +12,13 @@ namespace CurrencyConverter
     {
         static void Main(string[] args)
         {
+            string path = "C:/Users/victor.hearn-yeates/Source/Repos/CurrencyConverter/CurrencyConverter/CurrencyConverter/bin/Debug/eurocurrencies.xml";
+
+            XML xmlTest = new XML();
+
+            xmlTest.ReadXmlFile(path);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ CurrencyConverter/CurrencyConverter/XML.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CurrencyConverter
+{
+    public class XML
+    {
+        public void ReadXmlFile(string filename)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filename);
+
+            XmlNodeList nodeList = doc.DocumentElement.ChildNodes;
+
+            foreach (XmlNode n in nodeList)
+            {
+                if (n.Name == "Cube")
+                {
+                    foreach (XmlNode n2 in n.ChildNodes)
+                    {
+                        string time = n2.Attributes[0].Value;
+                        Console.WriteLine(time);
+                        foreach (XmlNode n3 in n2.ChildNodes)
+                        {
+                            string currency = n3.Attributes[0].Value;
+                            //Console.WriteLine(currency);
+                            string rate = n3.Attributes[1].Value;
+                            //Console.WriteLine(rate);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
