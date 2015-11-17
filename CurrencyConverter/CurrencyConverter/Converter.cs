4b925cc684cff72bcc0031728ed9afd8ec0e7db0
jamesfreke <jamesfreke2808@gmail.com>
2015-11-17 15:12:45 +0000

    ConvertTo tests added

---
+++ CurrencyConverter/CurrencyConverter/Converter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    public class Converter 
+    {
+
+        public double convertTo(string symbol, string symbol2, double value)
+        {
+            return value;            
+        }
+    }
+}
