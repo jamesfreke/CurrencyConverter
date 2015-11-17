29588bde373d6e325489c5d827c34454a38dffb4
LordStrange <victoryeates@googlemail.com>
2015-11-17 17:23:22 +0000

    Added averages, uncommented XML

---
+++ CurrencyConverter/CurrencyConverter/Average.cs
@@ -0,0 +1,16 @@
+﻿using CurrencyConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject
+{
+    public class Average : AvaliableFunctions
+    {
+        public List<Currency> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CurrencyConverter/UnitTestProject/AverageTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CurrencyConverter;
+using System.Collections.Generic;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class AverageTest
+    {
+        [TestMethod]
+        public void Test_GetList_TestsTheGetListMethodGetsAList_ReturnTypeList()
+        {
+            //Arrange
+            Average average = new Average();
+
+            //Act
+            List<Currency> currencyList = average.GetList();
+
+            //Assert
+
+        }
+    }
+}
