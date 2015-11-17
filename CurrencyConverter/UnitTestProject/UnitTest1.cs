abf6a87a9c9284cac53702cc811c0658bb88183c
LordStrange <victoryeates@googlemail.com>
2015-11-17 11:28:21 +0000

    Added Test 1

---
+++ CurrencyConverter/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_XMLRetriever_TestThatXMLRetrieverCallsTheGetXMLMethod_ReturnsAString()
+        {
+            //Arrange
+            XMLRetriever retriever = new XMLRetriever();
+            string expected = string.Empty;
+
+            //Act
+            string xml = retriever.GetXML();
+
+            //Assert
+            Assert.AreSame(expected, xml);
+        }
+    }
+}
---
+++ CurrencyConverter/UnitTestProject/XMLRetriever.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject
+{
+    public class XMLRetriever
+    {
+        internal string GetXML()
+        {
+            string result = string.Empty;
+            return result;
+        }
+    }
+}
